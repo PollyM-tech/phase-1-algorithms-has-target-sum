a0fd6e24464dc2af2d860bb8273a127706739a08
Pauline Moraa <pmoraa431@gmail.com>
2025-03-29 16:16:43 +0300

    Practice algorithmic problem solving Identify the Big O of an algorithm

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[i] + array[j] === target) return true;
+      }
+    }
+    return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+   O(n²) time, O(1) space
 */
 
 /* 
   Add your pseudocode here
+  1. Create an empty Set to track seen numbers
+  2. For each number in the array:
+     a. Calculate the complement (target - current number)
+     b. If complement exists in Set, return true
+     c. Add current number to Set
+  3. If loop completes, return false
 */
 
 /*
   Add written explanation of your solution here
+   This solution efficiently checks for pairs by storing numbers we've already seen.
+  For each number, we calculate what number would make the target sum (complement).
+  If we've seen that complement before, we return true. Otherwise, we store the
+  current number and continue. This approach only requires one pass through the array.
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +46,19 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-1, 0, 1], 0));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([], 10));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([10], 20));
 }
 
 module.exports = hasTargetSum;
